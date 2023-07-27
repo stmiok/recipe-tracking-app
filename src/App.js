e179949b716e97d1455c0a74e82a0c4477978151
Sam M <128644466+stmiok@users.noreply.github.com>
2023-07-27 14:32:29 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./App.css";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData"
+
+function App() {
+  const [recipes, setRecipes] = useState(RecipeData);
+
+  // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
+  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+
+  const createRecipe = (newRecipe) => 
+  setRecipes((currentRecipes) => [
+    ...currentRecipes,
+    newRecipe
+  ]);
+
+  const deleteRecipe = (indexToBeDeleted) =>
+  setRecipes((currentRecipes) => 
+    currentRecipes.filter((recipe, index) => index !== indexToBeDeleted)
+  );
+  
+  return (
+    <div className="App">
+      <header><h1>DELICIOUS FOOD RECIPES</h1></header>
+      <RecipeList 
+      recipes={recipes}
+      deleteRecipe={deleteRecipe}
+      />
+      <RecipeCreate 
+        createRecipe={createRecipe}
+      />
+    </div>
+  );
+}
+
+export default App;
